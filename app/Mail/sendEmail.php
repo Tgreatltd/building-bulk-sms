3446ad3f5651a7cd7660ce3fec1fdb9c3be8444c
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-19 11:28:31 -0400

    new commit

--- app/Mail/sendEmail.php
+++ app/Mail/sendEmail.php
@@ -41,7 +41,8 @@ class sendEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            // view: 'view.name',
+            view: 'mail',
         );
     }
 
