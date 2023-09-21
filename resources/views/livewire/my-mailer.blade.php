315914a7d898e255d34747753c0f6755d0f9846d
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-21 10:41:27 -0400

    new commit

---
+++ resources/views/livewire/my-mailer.blade.php
@@ -0,0 +1,13 @@
+<div>
+    {{-- In work, do what you enjoy. --}}
+    <div class="container mx-auto p-4">
+        <div class="bg-white p-6 rounded-lg shadow">
+            {{-- <h1 class="text-2xl font-semibold mb-4">Hello, {{Auth::user()->name}}</h1> --}}
+            <h1 class="text-2xl font-semibold mb-4">Hello</h1>
+            <p class="text-gray-700">Thank you for signing to our website</p>
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded mt-4">
+                Click Me
+            </button>
+        </div>
+    </div>
+</div>
