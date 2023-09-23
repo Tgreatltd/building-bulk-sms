f18f2f4cbc3377666c0c04f4c2457e749b1800fb
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-23 16:46:00 -0400

    new commmit

--- app/Livewire/Sendsms.php
+++ app/Livewire/Sendsms.php
@@ -18,7 +18,6 @@ class Sendsms extends Component
     {
         return view('livewire.sendsms');
         return view('livewire.sendsms');
-        return view('livewire.sendsms');
     }
     public function sendSms(){
         $this->info='';
