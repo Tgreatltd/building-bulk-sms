855132d9bd7ffccc857f1215d1093d97f687ea1e
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-25 05:44:59 -0400

    new commit

--- app/Livewire/MyPhonebook.php
+++ app/Livewire/MyPhonebook.php
@@ -6,8 +6,13 @@ use Livewire\Component;
 
 class MyPhonebook extends Component
 {
+    public $number, $name;
     public function render()
     {
         return view('livewire.myphonebook');
+    }
+
+    public function saveContact(){
+        dd($this->number);
     }
 }
