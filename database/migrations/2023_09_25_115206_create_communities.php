2ae561435936567c6a071250aa9553cbe89d8f31
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-25 08:00:36 -0400

    new commit

--- database/migrations/2023_09_25_115206_create_communities.php
+++ database/migrations/2023_09_25_115206_create_communities.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('community', function (Blueprint $table) {
+        Schema::create('communities', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('number');
             $table->timestamps();
         });
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('community');
+        Schema::dropIfExists('communities');
     }
 };
