cd7f6efa611221ec4dbb6fbd13e82c64572c27e7
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-27 03:57:59 -0400

    new commit

--- app/Livewire/Editphonebook.php
+++ app/Livewire/Editphonebook.php
@@ -8,15 +8,18 @@ use Livewire\Component;
 
 class Editphonebook extends Component
 {
-public $used,  $communityId;
+public $used, $id;
+
+const $communityId=$id;
 
 public function mount($id){
     $this->used = Community::find($id);
-    $this->communityId=$id;
+   ;
     
 }
     public function render()
     {
+
         return view('livewire.editphonebook');
     }
 }
