4a13ea122059c33d08a548043f7c0e36de93fa1c
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-28 09:43:49 -0400

    new commit

--- database/migrations/2023_09_28_132509_create_phonebooks.php
+++ database/migrations/2023_09_28_132509_create_phonebooks.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('phonebooks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('users_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
