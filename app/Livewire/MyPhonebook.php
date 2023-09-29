b469856f257e72f416a800f125d5f3af0b0d224a
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-29 18:29:57 -0400

    new commit

--- app/Livewire/MyPhonebook.php
+++ app/Livewire/MyPhonebook.php
@@ -37,16 +37,15 @@ public function saveNumber($id){
  $this->createNumber=Phonebook::find($id);
  if ($this->createNumber) {
     $this->phonebookId=$this->createNumber->id;
-    Phonebook::create(
+    Phonenumber::create(
         [ 
         
-        'name'=>$this->name,
+        'phoneNumber'=>$this->phoneNumber,
         'phone_id'=>$this->phonebookId,
         ]
     );
- } else{
-    echo 'invalid';
- }
+ } 
+ $this->phoneNumber = '';
 
 session()->flash('success', 'Your Number have been saved successfully');
 }
