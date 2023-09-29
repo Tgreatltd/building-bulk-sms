194400cb9bfbf5984e3d90e5fd66bff85eadd8ae
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-29 01:49:49 -0400

    new commit

--- resources/views/livewire/myphonebook.blade.php
+++ resources/views/livewire/myphonebook.blade.php
@@ -47,4 +47,28 @@
         </tbody>
     </table> 
 </div>
+
+
+<div class="container mx-auto p-4">
+    <form action="{{ route('save-data') }}" method="POST" class="max-w-md mx-auto">
+        @csrf
+
+        <div class="mb-4">
+            <label for="number" class="block text-gray-600 text-sm font-semibold">Number</label>
+            <input type="number" id="number" name="number" class="form-input">
+        </div>
+
+        <div class="mb-4">
+            <label for="id" class="block text-gray-600 text-sm font-semibold">ID</label>
+            <input type="text" id="id" name="id" class="form-input">
+        </div>
+
+        <div class="mb-4">
+            <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">
+                Save
+            </button>
+        </div>
+    </form>
+</div>
+
 </div>
