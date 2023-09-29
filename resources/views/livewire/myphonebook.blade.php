c2a668959499f965cae3696d4e5bf60f0a4b344b
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-29 02:14:55 -0400

    new commit

--- resources/views/livewire/myphonebook.blade.php
+++ resources/views/livewire/myphonebook.blade.php
@@ -1,6 +1,8 @@
-<div>
-    <div x-data="name">
-    <div class="container mx-auto mt-20 max-w-4xl mx-auto bg-white p-6 rounded shadow-md">
+<div x-data="{name:false}">
+    <template x-if="open">
+
+
+        <div class="container mx-auto mt-20 max-w-4xl mx-auto bg-white p-6 rounded shadow-md">
     <form wire:submit.prevent="save">
         <div class="mb-4">
             <label for="name" class="block text-gray-600 text-sm font-semibold">Name</label>
@@ -16,8 +18,8 @@
    </div>
 
 
-   <div class="container mx-auto mt-20 max-w-4xl mx-auto bg-white p-6 rounded shadow-md" x-data="{name:true}">
-     <h2 class="text-2xl font-semibold mb-4">PHONEBOOK LIST</h2> 
+   <div class="container mx-auto mt-20 max-w-4xl mx-auto bg-white p-6 rounded shadow-md">
+    <h2 class="text-2xl font-semibold mb-4">PHONEBOOK LIST</h2>
     <table class="min-w-full table" style="border: 2px solid black">
         <thead style="border: 2px solid black">
             <tr style="border: 2px solid black">
@@ -46,29 +48,30 @@
             @endforeach
         </tbody>
     </table> 
-</div>
+      </template>
+   
 
+    <div class="container mx-auto p-4" x-show="name">
+        <form action="" method="POST" class="max-w-md mx-auto">
+            @csrf
+    
+            <div class="mb-4">
+                <label for="number" class="block text-gray-600 text-sm font-semibold">Number</label>
+                <input type="number" id="number" name="number" class="form-input">
+            </div>
+    
+            <div class="mb-4">
+                <label for="id" class="block text-gray-600 text-sm font-semibold">ID</label>
+                <input type="text" id="id" name="id" class="form-input">
+            </div>
+    
+            <div class="mb-4">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">
+                    Save
+                </button>
+            </div>
+        </form>
+    </div>
 
-<div class="container mx-auto p-4">
-    <form action="" method="POST" class="max-w-md mx-auto">
-        @csrf
-
-        <div class="mb-4">
-            <label for="number" class="block text-gray-600 text-sm font-semibold">Number</label>
-            <input type="number" id="number" name="number" class="form-input">
-        </div>
-
-        <div class="mb-4">
-            <label for="id" class="block text-gray-600 text-sm font-semibold">ID</label>
-            <input type="text" id="id" name="id" class="form-input">
-        </div>
-
-        <div class="mb-4">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">
-                Save
-            </button>
-        </div>
-    </form>
-</div>
 
 </div>
