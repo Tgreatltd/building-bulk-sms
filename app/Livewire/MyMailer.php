4c6dc2b58807dc632838404113600f110d4d4711
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-01 18:00:39 -0400

    new commit

--- app/Livewire/MyMailer.php
+++ app/Livewire/MyMailer.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class MyMailer extends Component
 {
-    public $email, $message, $subject, $info;
+    public $email, $message, $subject, $info, $emailArray;
     public function render()
     {
         return view('livewire.my-mailer');
@@ -21,7 +21,11 @@ class MyMailer extends Component
             'subject' => 'required|string',
             'message' => 'required|string|',
         ]);
-        $data = ['subject' => $this->subject, 'message' => $this->message, 'email' => $this->email];
+        $emailArray=explode(',', $this->email);
+        foreach ($emailArray as $mail) {
+            $data = ['subject' => $this->subject, 'message' => $this->message, 'email' => $emailArray]; 
+        }
+      
         dispatch(new sendEmailJob($data));
         // Mail::to('olamoyeguntimothy@gmail.com')->send(new sendEmail());
         dd('Sent');
