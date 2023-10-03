d76e8b7d5e4a2b4d3eda5d97ce260444ed0906ab
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-03 03:27:17 -0400

    new commit

--- app/Livewire/MyPhonebook.php
+++ app/Livewire/MyPhonebook.php
@@ -10,58 +10,60 @@ use Livewire\Component;
 
 class MyPhonebook extends Component
 {
-    public $number, $name, $data,  $users, $ids, $phoneNumber, 
-    $phonebookId, $phone, $contacts, $allContact, $createNumber;
+    public $number, $name, $data,  $users, $ids, $phoneNumber,
+        $phonebookId, $phone, $contacts, $allContact, $createNumber;
 
-   
 
-    public function saveName(){
+
+    public function saveName()
+    {
         $this->validate([
             // 'name'=> 'required|string|unique:phonebooks,name',
-            'name'=> 'required|string',
+            'name' => 'required|string',
         ]);
-      $user_id= Auth::id();
+        $user_id = Auth::id();
         Phonebook::create(
-            [ 
-            
-            'name'=>$this->name,
-            'user_id'=>$user_id,
+            [
+
+                'name' => $this->name,
+                'user_id' => $user_id,
             ]
         );
-        session()->flash('success','Your Name have been saved');
+        session()->flash('success', 'Your Name have been saved');
     }
 
-public function saveNumber($id){
-    $this->validate([
-        'phoneNumber'=> 'required|string',
-     ]);
- $this->createNumber=Phonebook::find($id);
- if ($this->createNumber) {
-    $this->phonebookId=$this->createNumber->id;
-    Phonenumber::create(
-        [ 
-        
-        'phoneNumber'=>$this->phoneNumber,
-        'phone_id'=>$this->phonebookId,
-        ]
-    );
- } 
-
-session()->flash('success', 'Your Number have been saved successfully');
-}
-
-public function editContact($id){
-    $this->contacts = Phonebook::find($id);
-if ($this->contacts) {
-    $this->ids=$this->contacts->id;
-}
-}
-
-public function render()
+    public function saveNumber($id)
     {
-        $userId= Auth::id();
-        $users= Phonebook::where('user_id',$userId)->get();
-        return view('livewire.myphonebook', ['user'=>$users]);
+        $this->validate([
+            'phoneNumber' => 'required|string',
+        ]);
+        $this->createNumber = Phonebook::find($id);
+        if ($this->createNumber) {
+            $this->phonebookId = $this->createNumber->id;
+            Phonenumber::create(
+                [
+
+                    'phoneNumber' => $this->phoneNumber,
+                    'phone_id' => $this->phonebookId,
+                ]
+            );
+        }
+
+        session()->flash('success', 'Your Number have been saved successfully');
     }
 
+    public function editContact($id)
+    {
+        $this->contacts = Phonebook::find($id);
+        if ($this->contacts) {
+            $this->ids = $this->contacts->id;
+        }
+    }
+
+    public function render()
+    {
+        $userId = Auth::id();
+        $users = Phonebook::where('user_id', $userId)->get();
+        return view('livewire.myphonebook', ['user' => $users,]);
+    }
 }
