69f2307ccd3c82777c4e7f95db24c84d5fb7f898
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-14 12:18:58 -0400

    new commit

--- resources/views/livewire/editphonebook.blade.php
+++ resources/views/livewire/editphonebook.blade.php
@@ -7,12 +7,10 @@
         <h2 class="text-2xl font-semibold mb-4">Update Form</h2>
         <form > <!-- Replace 'update.route' with your actual update route -->
             @csrf <!-- Add a CSRF token field for security -->
-            @if ($user)
             <div class="mb-4">
                 <label for="input1" class="block text-gray-600">ID</label>
                 <input disabled type="text" name='id' wire:model='id' value="{{$user->id}}" class="border rounded w-full py-2 px-3 focus:outline-none focus:border-blue-400">
-            </div>  
-            @endif
+            </div>
             <div class="mb-4">
                 <label  class="block text-gray-600">NAME</label>
                 <input name="name" type="text"  wire:model='name' value="" class="border rounded w-full py-2 px-3 focus:outline-none focus:border-blue-400">
