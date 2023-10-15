12d74de387b52a0b2fff5f42da07c95a689b483c
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-15 16:55:08 -0400

    new commit

--- app/Livewire/Sendsms.php
+++ app/Livewire/Sendsms.php
@@ -38,7 +38,7 @@ class Sendsms extends Component
 
        foreach($numbersArray as $numba){
             // $dumpedNumbers[]= $numba;
-            dd($numbersArray, $this->sender,$this->message );
+            // dd($numbersArray, $this->sender,$this->message );
         }
         
         // dump all the numbers at once after looping
@@ -48,7 +48,7 @@ class Sendsms extends Component
         $client = new Client();
         $response=$client->post('http://www.bulksmsnigeria.com/api/v1/sms/create?api_token=t8NbaYdc5W5WGMk4MXy9mAypdgY2cJEAgRfJI8ZdiSzFY6ZdNEW1l6skgg0v
         &from='.$this->sender.'&to='.$this->number.'&body='.$this->message);
-        $result=$response
+        $result=$response->getBody()->getContents();
         
      
          $this->info='message sent successfully';
