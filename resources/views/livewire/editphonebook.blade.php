ca63fa0460b564b8481ca2f2d4a2902d72811724
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-20 14:46:07 -0400

    new commit

--- resources/views/livewire/editphonebook.blade.php
+++ resources/views/livewire/editphonebook.blade.php
@@ -16,11 +16,11 @@
             <div class="mb-4">
                 <label  class="block text-gray-600">NAME</label>
                 <input name="name" type="text"  wire:model='name' value="{{$phonebookId->name}}" class="border rounded w-full py-2 px-3 focus:outline-none focus:border-blue-400">
-            </div>
-            <div class="mb-4">
+            </div> 
+            {{-- <div class="mb-4">
                 <label for="input3" class="block text-gray-600">NUMBERS</label>
-                <input type="text" name="number"   wire:model='number' value="" class="border rounded w-full py-2 px-3 focus:outline-none focus:border-blue-400">
-            </div>
+                <input type="text" name="number"   wire:model='number' value="{{$phonebookId->phoneNumber}}" class="border rounded w-full py-2 px-3 focus:outline-none focus:border-blue-400">
+            </div> --}}
             <div class="text-center">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue active:bg-blue-700">Update</button>
             </div>
