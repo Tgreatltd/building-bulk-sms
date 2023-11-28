f50d5de8c0eca72c98c3827785b9e7098d2f324b
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-11-28 19:18:39 +0100

    new commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,7 +37,7 @@
                 {{-- <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}"> --}}
                 <a class="" href="{{ route('register') }}">
                     {{ __('Sign Up') }}
-                </a>
+                </a> 
             </x-primary-button>
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" w>
                         {{ __('Dashboard') }}
                         {{-- <h3 class=" m-20">#{{Auth::user()->wallet}}</h3>  --}}
                     </x-nav-link>
